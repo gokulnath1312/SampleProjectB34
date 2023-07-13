bfe564b2720f914046ffb1e54f5104f09918c978
Administrator <Administrator@172.31.22.230>
2023-07-13 08:07:29 +0000

    Drag and drop

--- src/test/java/testScript/SeleniumDragandDrop.java
+++ src/test/java/testScript/SeleniumDragandDrop.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-public class SeleniumActionDemo {
+public class SeleniumDragandDrop {
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
@@ -22,7 +22,7 @@ public class SeleniumActionDemo {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 //		driver.get("https://www.google.com/");
-		driver.get("https://testautomationpractice.blogspot.com/");
+		driver.get("https://demo.opencart.com/");
 		Actions action = new Actions(driver);
 		WebElement searchbox=driver.findElement(By.id("search"));
 		
