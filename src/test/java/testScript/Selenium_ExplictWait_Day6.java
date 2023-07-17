3fe097f9a7c75dd36019d19cfc0ad98e07ba7077
GokulNath <GokulNath@Gokulnath>
2023-07-17 00:04:53 -0500

    Day 6 changes

---
+++ src/test/java/testScript/Selenium_ExplictWait_Day6.java
@@ -0,0 +1,32 @@
+package testScript;
+
+import java.time.Duration;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Selenium_ExplictWait_Day6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		WebDriverWait wait= new WebDriverWait(driver,Duration.ofSeconds(5));
+		driver.manage().window().maximize();
+		driver.get("https://demo.seleniumeasy.com/javascript-alert-box-demo.html");
+//		driver.findElement(By.id("username")).sendKeys("tomsmith");
+		driver.findElement(By.xpath("//button[@class='btn btn-default']")).click();
+		wait.until(ExpectedConditions.alertIsPresent());
+		Alert alert=driver.switchTo().alert();
+		System.out.println("Alert text is..."+alert.getText());
+		alert.accept();
+		driver.quit();
+
+	}
+
+}
--- src/test/java/testScript/Selenium_ImplictWait_Day6.java
+++ src/test/java/testScript/Selenium_ImplictWait_Day6.java
@@ -1,18 +1,15 @@
 package testScript;
 
-import java.time.Duration;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class TestLoginSample {
+public class Selenium_ImplictWait_Day6 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		WebDriver driver=new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		driver.get("https://www.google.com");
 		driver.findElement(By.name("q")).sendKeys("github");
 		driver.quit();
